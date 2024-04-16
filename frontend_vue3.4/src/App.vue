bf615e74c5546bfc6e1198b2d19cffdede8b1b9d
mrzou <382066178@qq.com>
2024-04-16 22:50:22 +0800

    使用本地font

--- frontend_vue3.4/src/App.vue
+++ frontend_vue3.4/src/App.vue
@@ -9,7 +9,8 @@
 <style>
 @import '@/assets/viewwindow.css';
 @import '@/assets/reset.css';
-@import '//at.alicdn.com/t/c/font_4507918_4ws7eimwc2y.css';
+/* @import '//at.alicdn.com/t/c/font_4507918_4ws7eimwc2y.css'; */
+@import '@/assets/font/iconfont.css';
 
 body {
   user-select: none;
