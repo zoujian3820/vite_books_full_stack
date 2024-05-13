1e04aa1a23b34d67e6fb8f7ebcf8e915e72ed44d
mrzou <382066178@qq.com>
2024-05-13 16:45:06 +0800

    upt

--- frontend_vue3.4/vite.config.ts
+++ frontend_vue3.4/vite.config.ts
@@ -90,7 +90,51 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       // 关闭资源转base64
-      assetsInlineLimit: 0
+      assetsInlineLimit: 0,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            // 把node_modules中第三方的包，按模块拆分成单个的代码块文件
+            // element-plus打包出来、包特大，本想单独做了细致拆分，按组件名拆分包，一个组件一个文件
+            // 但会打包后页面会产生报错白屏，应该是名字改了后，文件之间的依赖关系变了，导致找不到文件
+            if (id.includes('node_modules')) {
+              // element-plus/es/index.mjs
+              // element-plus/es/constants/aria.mjs
+              // element-plus/es/components/table/src/store/current.mjs
+              const path_ = id.toString().split('node_modules/')[1].toString()
+              // element-plus
+              const chunkName = path_.split('/')[0].toString()
+              return chunkName
+
+              // if (chunkName === 'element-plus') {
+              //   // 适用这种组件element-plus/es/components/table/src/store/current.mjs
+              //   const eplCompReg = /^element-plus\/es\/components\/(.)*\/src\/(.)*/
+              //   // 创建一个正则表达式，用于匹配并删除开头的路径部分
+              //   const stReg = /^element-plus\/es\/components\//
+              //   // 创建一个正则表达式，用于匹配并删除结尾的文件扩展名部分
+              //   const edReg = /\/src\/(.)*/gm
+
+              //   // 适用这种组件element-plus/es/constants/aria.mjs  element-plus/es/index.mjs
+              //   const stReg_ = /^element-plus\/es\//
+
+              //   if (eplCompReg.test(path_)) {
+              //     const compName = path_.replace(stReg, '').replace(edReg, '')
+              //     return `${chunkName}_${compName}`
+              //   } else {
+              //     const ph_str = path_.replace(stReg_, '')
+              //     if (ph_str.indexOf('/') !== -1) {
+              //       return chunkName + '_' + ph_str.split('/')[0]
+              //     } else {
+              //       return chunkName + '_' + ph_str.split('.')[0]
+              //     }
+              //   }
+              // } else {
+              //   return chunkName
+              // }
+            }
+          }
+        }
+      }
     }
   }
 })
